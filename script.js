c7f9a32dbacaf86e985cd153258d9a6cb04232de
=caoanhquan <caoanhquan_qn@yahoo.com>
2021-01-04 14:10:00 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,104 @@
+"use strict";
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: "Jonas Schmedtmann",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Sarah Smith",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector(".welcome");
+const labelDate = document.querySelector(".date");
+const labelBalance = document.querySelector(".balance__value");
+const labelSumIn = document.querySelector(".summary__value--in");
+const labelSumOut = document.querySelector(".summary__value--out");
+const labelSumInterest = document.querySelector(".summary__value--interest");
+const labelTimer = document.querySelector(".timer");
+
+const containerApp = document.querySelector(".app");
+const containerMovements = document.querySelector(".movements");
+
+const btnLogin = document.querySelector(".login__btn");
+const btnTransfer = document.querySelector(".form__btn--transfer");
+const btnLoan = document.querySelector(".form__btn--loan");
+const btnClose = document.querySelector(".form__btn--close");
+const btnSort = document.querySelector(".btn--sort");
+
+const inputLoginUsername = document.querySelector(".login__input--user");
+const inputLoginPin = document.querySelector(".login__input--pin");
+const inputTransferTo = document.querySelector(".form__input--to");
+const inputTransferAmount = document.querySelector(".form__input--amount");
+const inputLoanAmount = document.querySelector(".form__input--loan-amount");
+const inputCloseUsername = document.querySelector(".form__input--user");
+const inputClosePin = document.querySelector(".form__input--pin");
+
+containerApp.style.opacity = "1";
+const displayMovement = function (val, i) {
+  const movement = document.createElement("div");
+  movement.classList.add("movements__row");
+  const child1 = document.createElement("div");
+  const child2 = document.createElement("div");
+  const child3 = document.createElement("div");
+  child1.classList.add("movements__type");
+  if (val > 0) {
+    child1.classList.add("movements__type--deposit");
+    child1.textContent = `${i + 3} deposit`;
+  } else {
+    child1.classList.add("movements__type--withdrawal");
+    child1.textContent = `${i + 3} withdrawal`;
+  }
+  child2.classList.add("movements__date");
+  child3.classList.add("movements__value");
+  child3.textContent = `${val} €`;
+  movement.appendChild(child1);
+  movement.appendChild(child2);
+  movement.appendChild(child3);
+  containerMovements.prepend(movement);
+};
+
+let total = 3622;
+let cashIn = 4000;
+let cashOut = -378;
+account1.movements.forEach(function (value, index, _) {
+  displayMovement(value, index);
+  total += value;
+  labelBalance.textContent = `${total} €`;
+  if (value > 0) {
+    cashIn += value;
+    console.log(value, cashIn);
+  } else if (value < 0) {
+    cashOut += value;
+    console.log(value, cashOut);
+  }
+  labelSumIn.textContent = cashIn;
+  labelSumOut.textContent = cashOut;
+});
