90cad6daca6b347d7b1a42c17379d9ae79b3a1a6
=caoanhquan <caoanhquan_qn@yahoo.com>
2021-01-09 10:38:09 +1100

    add transaction date and format number

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
@@ -17,6 +30,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account3 = {
@@ -24,6 +50,19 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account4 = {
@@ -31,6 +70,16 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -81,8 +130,10 @@ const displayAccount = function (account, sort = false) {
     <div class="movements__type movements__type--${type}">${
       index + 1
     } ${type}</div>
-    <div class="movements__date">3 days ago</div>
-    <div class="movements__value">${value} €</div>`;
+    <div class="movements__date">${new Date(
+      account.movementsDates[index]
+    ).toLocaleDateString()}</div>
+    <div class="movements__value">${value.toFixed(2)} €</div>`;
     containerMovements.insertAdjacentHTML("afterbegin", movementRow);
   });
 
@@ -90,25 +141,25 @@ const displayAccount = function (account, sort = false) {
     (accumulator, value) => accumulator + value,
     0
   );
-  labelBalance.textContent = `${balance} €`;
+  labelBalance.textContent = `${balance.toFixed(2)} €`;
 
   const deposit = account.movements
     .filter((value) => value > 0)
     .reduce((accumulator, value) => accumulator + value, 0);
-  labelSumIn.textContent = `${deposit} €`;
+  labelSumIn.textContent = `${deposit.toFixed(2)} €`;
 
   const withdrawal = account.movements
     .filter((value) => value < 0)
     .reduce((accumulator, value) => accumulator + value, 0);
 
-  labelSumOut.textContent = `${Math.abs(withdrawal)} €`;
+  labelSumOut.textContent = `${Math.abs(withdrawal).toFixed(2)} €`;
 
   const surplus = account.movements
     .filter((mov) => mov > 0)
     .map((deposit) => (deposit * account.interestRate) / 100)
     .filter((int) => int >= 1)
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${surplus} €`;
+  labelSumInterest.textContent = `${surplus.toFixed(2)} €`;
 
   const firstName = account.owner.split(" ")[0];
   labelWelcome.textContent = `Welcome back, ${firstName}!`;
@@ -182,6 +233,11 @@ btnLogout.addEventListener("click", function () {
 
 //implement transfer
 
+function addTransaction(account, num) {
+  account.movements.push(num);
+  account.movementsDates.push(new Date());
+}
+
 btnTransfer.addEventListener("click", function (event) {
   event.preventDefault();
   const amount = Number(inputTransferAmount.value);
@@ -195,8 +251,8 @@ btnTransfer.addEventListener("click", function (event) {
       } else if (currentAccount.username === receivingAcc.username) {
         alert("Invalid receiving account ⛔️⛔️⛔️!!!");
       } else {
-        receivingAcc.movements.push(amount);
-        currentAccount.movements.push(amount * -1);
+        addTransaction(currentAccount, -amount);
+        addTransaction(receivingAcc, amount);
         displayAccount(currentAccount);
       }
     } else {
@@ -241,7 +297,7 @@ btnLoan.addEventListener("click", function (event) {
   const checkedCondition = deposit.some((mov) => mov >= loanAmout * 0.1);
   if (loanAmout > 0) {
     if (checkedCondition) {
-      currentAccount.movements.push(loanAmout);
+      addTransaction(currentAccount, loanAmout);
       alert("You are granted this loan 🎉🎉🎉!!!");
       displayAccount(currentAccount);
     } else {
@@ -253,21 +309,6 @@ btnLoan.addEventListener("click", function (event) {
 
   clearLoanValue();
 });
-
-// sorting
-
-// btnSort.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   const sortedMovements = [...currentAccount.movements].sort((a, b) => a - b);
-//   const currentAccountCopy = { ...currentAccount };
-//   currentAccountCopy.movements = sortedMovements;
-//   counter++;
-//   if (counter % 2 === 1) {
-//     displayAccount(currentAccountCopy);
-//   } else {
-//     displayAccount(currentAccount);
-//   }
-// });
 
 let sorted = false;
 btnSort.addEventListener("click", function (event) {
